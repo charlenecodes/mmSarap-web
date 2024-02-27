4996a924d3b9d28af07c16d09795f2fa6593cf10
Charlene <lavacharlene.dev@gmail.com>
2024-02-27 15:30:56 +0100

    Updates

--- src/PrivateRoutes.jsx
+++
@@ -1,5 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-
-export const PrivateRoutes = ({ isLoggedIn }) => {
-  return <div>{isLoggedIn ? <Outlet /> : <Navigate to="login" />}</div>;
-};
